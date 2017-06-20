b8ef6f76455eab233c225399dfebd34653faee3d
srikanth <srikanthkanjarla@gmail.com>
2017-06-20 22:39:06 +0530

    Update social link colors
    updated social link colors in footer
    small changes in theme.css to improve appearence

--- js/getquotes.js
+++ js/getquotes.js
@@ -1,5 +1,4 @@
 //API link to get random quote - 'https://forismatic.com/en/api/'
-
 var url = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 var getRandomQuote = function(data) {
     var quoteText = data.quoteText;
