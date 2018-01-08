59d8e9ddeab9e03da0ded0599b9950cfbc9e8456
srikanth <srikanthkanjarla@gmail.com>
2018-01-08 20:01:19 +0530

    remove w3c css validator code

--- js/getquotes.js
+++ js/getquotes.js
@@ -1,22 +1,54 @@
-//API link to get random quote - 'https://forismatic.com/en/api/'
-var url = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
-var getRandomQuote = function(data) {
-    var quoteText = data.quoteText;
-    var quoteAuthor = data.quoteAuthor;
-    $('#quote-text').text(quoteText);
-    $('#quote-author').text('-- ' + quoteAuthor);
-    if (quoteAuthor == '') { $('#quote-author').text('Unknown'); }
+/*
+var url = API link to get random quote - 'https://forismatic.com/en/api/'
+1.getQuote()    : get random quote from API source and call filterQuote()
+2.filterQuote() : if quote length is more than 140 characters get new quote  and call quoteData() 
+3.quoteData()  : add quote data to object and call viewQuote()  
+4.viewQuote()   : display quote to user
+*/
 
+
+var url = 'https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?';
+
+function getQuote() {
+    $.getJSON(url, filterQuote, 'jsonp');
+}
+
+function filterQuote(data) {
+    if ((data.quoteText).length > 139) {
+
+        getQuote();
+    } else {
+        quoteText(data);
+    }
+}
+
+function quoteText(data) {
+    //quote data object
+    var quoteObj = {
+        quote: data.quoteText,
+        quoteAuthor: data.quoteAuthor,
+        quoteHashTag: 'FreeCodeCamp,RandomQuoteMachine'
+    }
+    viewQuote(quoteObj);
+}
+
+
+function viewQuote(quoteObj) {
+    $('#quote-text').text(quoteObj.quote);
+    $('#quote-author').text('-- ' + quoteObj.quoteAuthor);
+    // if author data empty display unknown
+    if (quoteObj.quoteAuthor == '') { $('#quote-author').text('-- Unknown'); }
     //tweet quote on clicking tweet link
-    var quoteTweet = 'https://twitter.com/intent/tweet?text=' + quoteText + ' Author - ' + quoteAuthor + '&hashtags=FCC,RandomQuoteMachine';
+    var quoteTweet = 'https://twitter.com/intent/tweet?text=' + quoteObj.quote + ' - ' + quoteObj.quoteAuthor + '&hashtags=' + quoteObj.quoteHashTag + '';
     $('#tweet-quote').attr('href', quoteTweet);
 }
 
-$(function() {
-    $.getJSON(url, getRandomQuote, 'jsonp');
-    $('#get-quote').on('click', function(e) {
-        $.getJSON(url, getRandomQuote, 'jsonp'); //get next random quote on cliking next quote link
-        e.preventDefault(); //prevent default behaviour of anchor tag on click
-    });
-
+$('#get-quote').on('click', function(e) {
+    getquote();
+    e.preventDefault(); //prevent default behaviour of anchor tag on click
 });
+
+
+
+
+getQuote();
