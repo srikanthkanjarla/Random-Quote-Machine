0642b684bc8798f7bb3d64d497a501bfe0593096
srikanth kanjarla <srikanthkanjarla@gmail.com>
2018-10-10 19:52:10 +0530

    Add components folder, header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
+import Header from "./components/Header";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return <div className="App" />;
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
