c390cdeab9bbd85ccbd17c13ae5b14abc8d15636
srikanth kanjarla <srikanthkanjarla@gmail.com>
2018-10-18 21:54:45 +0530

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,20 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <h1 className="header-title">Random Quote Machine</h1>
+      <div className="header-text">
+        <span className="first-letter">Q</span>
+        <span>u</span>
+        <span>o</span>
+        <span>t</span>
+        <span>e</span>
+        <span className="first-letter">M</span>
+        <span>a</span>
+        <span>c</span>
+        <span>h</span>
+        <span>i</span>
+        <span>n</span>
+        <span>e</span>
+      </div>
     </header>
   );
 }
