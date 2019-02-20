348cfe3c7422be2a2b861067505449dc1be1994d
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-02-20 14:38:30 +0530

    Update removed multiple span tags in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,13 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
+  const headerText = "QuoteMachine";
   return (
     <header className="header">
       <div className="header-text">
-        <span className="first-letter">Q</span>
-        <span>u</span>
-        <span>o</span>
-        <span>t</span>
-        <span>e</span>
-        <span className="first-letter">M</span>
-        <span>a</span>
-        <span>c</span>
-        <span>h</span>
-        <span>i</span>
-        <span>n</span>
-        <span>e</span>
+        {headerText.split("").map(letter => (
+          <span>{letter}</span>
+        ))}
       </div>
     </header>
   );
